8b21b4882b8d18275ee2bb89d18fc308366a7e94
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-07-08 16:07:40 +0200

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <HeaderSpotify/>
+    
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <h1 class="display-1">Display 1</h1>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+import HeaderSpotify from './components/HeaderSpotify.vue'
+
 
 export default {
   name: 'App',
   components: {
+    HeaderSpotify
     // HelloWorld
   }
 }
 </script>
 
 <style lang="scss">
-#app {
   @import '@/style/general.scss';
 
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  #app {
+
+
+  }
 </style>
---
+++ src/components/HeaderSpotify.vue
@@ -0,0 +1,31 @@
+<template>
+    <header class="p-4">
+        <div class="box-logo d-inline-block rounded-circle">
+            <a href="#">
+                <i class="fab fa-spotify"></i>
+            </a>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name : 'HeaderSpotify'
+}
+</script>
+<style lang="scss" scoped>
+    header {
+        background: #2e3a46;
+
+        .box-logo {
+            background: black;
+
+            i {
+                color: #1dd760;
+                font-size: 50px;
+            }
+    }
+    }
+
+
+</style>
